9ee6fa1da47a9b2f8e25b1bab1826382961f3bd1
Em-Verdier <verdierem@gmail.com>
2020-09-29 11:26:27 +0200

    final

--- src/asyncPromise2.js
+++ src/asyncPromise2.js
@@ -4,14 +4,12 @@ import { COPYFILE_EXCL } from 'constants'
 const src = process.argv[2]
 const dest = process.argv[3]
 
-fsPromises
-    .copyFile(`../${src}`, `../${dest}`, COPYFILE_EXCL)
-    .then(() => console.log('source.txt was copied to destination.txt'))
-    .catch(() => console.log('The file could not be copied'))
-
 if (process.argv.length !== 4) {
     console.log('Usage: node asyncPromise.js src dest')
+    process.exit(1)
 }
-if (!isNaN(src) || !isNaN(dest)) {
-    console.log('Erreur: src <String> dest <String>')
-}
+
+fsPromises
+    .copyFile(src, dest, COPYFILE_EXCL)
+    .then(() => console.log('source.txt was copied to destination.txt'))
+    .catch(() => console.log('The file could not be copied'))
